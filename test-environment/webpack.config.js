e12484e32b395e22207d16cf81d3b9a642c49ab2
Drago Odalovic <Drago Odalovic>
2016-10-03 21:31:45 +0200

    doradi pakete kod templejta

--- test-environment/webpack.config.js
+++ test-environment/webpack.config.js
@@ -23,7 +23,10 @@ module.exports = {
         loaders: [ 'babel' ],
         exclude: /node_modules/,
         include: __dirname
-      }
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
   }
 }
