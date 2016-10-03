3cbbe0a387bc566fecf0e2db85e85a0e2df44ee7
Drago Odalovic <Drago Odalovic>
2016-10-03 22:29:05 +0200

    zakomentarisi console.log

--- comments-app/components/CommentList.js
+++ comments-app/components/CommentList.js
@@ -4,7 +4,7 @@ import Comment from './Comment'
 export default class CommentList extends React.Component {
     render() {
         const { podaciCommentList } = this.props;
-        console.log(podaciCommentList);
+        // console.log(podaciCommentList);
         const podaciKomentari = podaciCommentList.map(element => {
             return (
                 <Comment key={element.id} author={element.author}>
